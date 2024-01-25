d8b328b0b926e521a5c5d2fab6d20c502ea746dd
magistraapta <magsitraagis@gmail.com>
2024-01-25 11:24:11 +0800

    Initial Commit

---
+++ coba-testing/coba_testingApp.swift
@@ -0,0 +1,17 @@
+//
+//  coba_testingApp.swift
+//  coba-testing
+//
+//  Created by Magistra Apta on 25/01/24.
+//
+
+import SwiftUI
+
+@main
+struct coba_testingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
